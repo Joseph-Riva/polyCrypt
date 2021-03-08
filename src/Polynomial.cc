b7770546a7db5a021ebed6fefa80ee648a6337b1
Joseph Riva <josephariva@gmail.com>
2021-03-07 19:09:47 -0700

    file structure

---
+++ src/Polynomial.cc
@@ -0,0 +1,3 @@
+int add(){
+    return 1;
+}
---
+++ tests/test.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Polynomial.h"
+
+using namespace std;
+
+int main(){
+    cout << "hi" << '\n';
+    cout << add() << '\n';
+}
